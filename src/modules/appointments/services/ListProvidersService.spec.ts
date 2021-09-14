f4e3cc5ac8b3ef7f176cf821c566ea643dbc6fdd
Ana Carolina Ribeiro <carolribeiro2112@gmail.com>
2021-09-14 11:05:00 -0300

    providers service

---
+++ src/modules/appointments/services/ListProvidersService.spec.ts
@@ -0,0 +1,41 @@
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import ListProvidersService from './ListProvidersService';
+
+let fakeUsersRepository: FakeUsersRepository;
+let listProviders: ListProvidersService;
+
+describe('UpdateUserAvatar', ()=>{
+  beforeEach(()=>{
+    fakeUsersRepository = new FakeUsersRepository();
+    listProviders = new ListProvidersService(fakeUsersRepository);
+  })
+
+  it('should be able to list providers', async ()=>{
+    const user1 = await fakeUsersRepository.create({
+     name: 'John Doe',
+     email: 'johndoe@example.com',
+     password: '123456'
+    });
+
+    const user2 = await fakeUsersRepository.create({
+      name: 'John Smith',
+      email: 'johnsm@example.com',
+      password: '123456'
+    });
+
+    const loggedUser = await fakeUsersRepository.create({
+      name: 'Joseph Climber',
+      email: 'joeclimber@example.com',
+      password: '123456'
+    });
+
+    const providers = await listProviders.execute({
+      user_id: loggedUser.id,
+    })
+
+    expect(providers).toEqual([
+      user1,
+      user2
+    ]);
+  });
+});
