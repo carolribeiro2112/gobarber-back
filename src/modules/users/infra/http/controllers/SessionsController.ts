1218258991017aff045a6cf0b5ec3faa3d03b4e2
Ana Carolina Ribeiro <carolribeiro2112@gmail.com>
2021-09-17 13:38:33 -0300

    add class transformer

--- src/modules/users/infra/http/controllers/SessionsController.ts
+++ src/modules/users/infra/http/controllers/SessionsController.ts
@@ -1,7 +1,7 @@
 import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
 import { Request, Response } from 'express'
 import { container } from 'tsyringe';
-
+import { classToClass } from 'class-transformer';
 export default class SessionsController {
   public async create(request:Request, response:Response): Promise<Response> {
     const { email, password } = request.body;
@@ -13,9 +13,6 @@ export default class SessionsController {
       password,
     });
 
-    // @ts-expect-error
-    delete user.password;
-
-    return response.json({ user, token });
+    return response.json({ user: classToClass(user), token });
   }
 }
