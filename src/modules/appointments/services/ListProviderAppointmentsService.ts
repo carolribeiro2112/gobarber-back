e08e15e627d0cfe7db52d131969a966a8258a51c
Ana Carolina Ribeiro <carolribeiro2112@gmail.com>
2021-09-21 17:31:03 -0300

    cache config

--- src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -4,6 +4,8 @@ import { injectable, inject } from 'tsyringe';
 import Appointment from '../infra/typeorm/entities/Appointment';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+
 interface IRequest {
   provider_id:string;
   day:number;
@@ -16,15 +18,25 @@ class ListProviderAppointmentService {
   constructor(
     @inject('AppointmentsRepository')
     private appointmentsRepository: IAppointmentsRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) {}
 
   public async execute({ provider_id, day, month, year  }: IRequest): Promise<Appointment[]> {
+    const cacheData = await this.cacheProvider.recover('ahua')
+
+    console.log(cacheData);
+
     const appointments = await this.appointmentsRepository.findAllInDayFromProvider({
       provider_id,
       day,
       month,
       year
-    });
+    }
+  );
+
+  // await this.cacheProvider.save('ahua', 'hoho')
 
     return appointments;
   }
